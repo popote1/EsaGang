9a7d2fd8850710fd04ad7efeb7a2378028335d7c
popote <yvan.bladet@gmail.com>
2022-01-24 13:53:07 +0100

    FirstPush

---
+++ Assets/ShowOffScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOffScript : MonoBehaviour
+{
+
+    public int myint = 2;
+    public bool mybool;
+    public Rigidbody MyRigidbody;
+    void Update()
+    {
+
+        
+    }
+}
