daeab50f735c151759bfcabfd4b258e016cbf3a1
Drevok <theoartous@hotmail.fr>
2022-01-24 17:01:44 +0100

    Trying to make the keyboard working, nearly done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,10 +8,13 @@ public class PlayerController : MonoBehaviour
 {
     private float AxisX, AxisZ;
     private Rigidbody rb;
-    
+    PlayerInput playerInput;
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        playerInput = gameObject.GetComponent<PlayerInput>();
+        Debug.Log(playerInput.currentControlScheme);
     }
 
     // Update is called once per frame
@@ -19,9 +23,28 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(AxisX, 0, AxisZ);
     }
 
-    public void OnLeftStick(InputValue val)
+    public void OnMovement(InputValue val)
     {
-        AxisZ = val.Get<Vector2>().y;
-        AxisX = val.Get<Vector2>().x;
+        if (playerInput.currentControlScheme == "XboxController")
+        {
+            AxisZ = val.Get<Vector2>().y;
+            AxisX = val.Get<Vector2>().x;
+        }
+
+        if (playerInput.currentControlScheme == "KeyboardMouse")
+        {
+            AxisZ = val.Get<float>() * 5;
+        }
+    }
+
+    public void OnGrabObject(InputValue val)
+    {
+        Debug.Log("Je chope un objet");
+        rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+    }
+
+    public void OnHit(InputValue val)
+    {
+        Debug.Log("Je cogne dur");
     }
 }
